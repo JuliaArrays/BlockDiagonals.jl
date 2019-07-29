51a2f59248048ea2493e752701799cfc2fb982e4
Nick Robinson <npr251@gmail.com>
2019-07-29 13:36:14 -0500

    Enable doctests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
+using BlockDiagonals
+using Documenter
 using Test
 
 @testset "BlockDiagonals" begin
+    doctest(BlockDiagonals)
     include("blockdiagonal.jl")
     include("base_maths.jl")
     include("linalg.jl")
