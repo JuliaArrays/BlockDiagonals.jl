8c4e73b8e68abce96600def665d78ab06a6c2d1b
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-09-15 16:38:37 +0200

    Parametrize vector in BlockDiagonal

--- src/blockdiagonal.jl
+++ src/blockdiagonal.jl
@@ -5,16 +5,16 @@
 
 A matrix with matrices on the diagonal, and zeros off the diagonal.
 """
-struct BlockDiagonal{T, V<:AbstractMatrix{T}} <: AbstractMatrix{T}
-    blocks::Vector{V}
+struct BlockDiagonal{T, V<:AbstractMatrix{T}, AV<:AbstractVector{V}} <: AbstractMatrix{T}
+    blocks::AV
 
-    function BlockDiagonal{T, V}(blocks::Vector{V}) where {T, V<:AbstractMatrix{T}}
-        return new{T, V}(blocks)
+    function BlockDiagonal{T, V, AV}(blocks::AV) where {T, V<:AbstractMatrix{T}, AV<:AbstractVector{V}}
+        return new{T, V, AV}(blocks)
     end
 end
 
-function BlockDiagonal(blocks::Vector{V}) where {T, V<:AbstractMatrix{T}}
-    return BlockDiagonal{T, V}(blocks)
+function BlockDiagonal(blocks::AV) where {T, V<:AbstractMatrix{T}, AV<:AbstractVector{V}}
+    return BlockDiagonal{T, V, AV}(blocks)
 end
 
 BlockDiagonal(B::BlockDiagonal) = B
