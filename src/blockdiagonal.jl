6f974973e91b272a4823125ae7358fddcf33875e
wt <wt0881@my.bristol.ac.uk>
2020-12-02 13:39:45 +0000

    Avoid closing over B

--- src/blockdiagonal.jl
+++ src/blockdiagonal.jl
@@ -108,19 +108,19 @@ end
 ## Base
 Base.Matrix(B::BlockDiagonal) = cat(blocks(B)...; dims=(1, 2))
 
-function ChainRulesCore.rrule(::Type{<:Base.Matrix}, B::BlockDiagonal)
+function ChainRulesCore.rrule(::Type{<:Base.Matrix}, B::T) where {T<:BlockDiagonal}
+    nrows = size.(B.blocks, 1)
+    ncols = size.(B.blocks, 2)
     function Matrix_pullback(Δ::AbstractMatrix)
-        nrows = size.(B.blocks, 1)
-        ncols = size.(B.blocks, 2)
         row_idxs = cumsum(nrows) .- nrows .+ 1
         col_idxs = cumsum(ncols) .- ncols .+ 1
 
-        Δblocks = map(eachindex(B.blocks)) do n
+        Δblocks = map(eachindex(nrows)) do n
             block_rows = row_idxs[n]:(row_idxs[n] + nrows[n] - 1)
             block_cols = col_idxs[n]:(col_idxs[n] + ncols[n] - 1)
             return Δ[block_rows, block_cols] 
         end
-        return (NO_FIELDS, Composite{typeof(B)}(blocks=Δblocks))
+        return (NO_FIELDS, Composite{T}(blocks=Δblocks))
     end
     return Matrix(B), Matrix_pullback
 end
