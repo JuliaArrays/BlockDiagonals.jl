97b5e9278ab55e9af64831947ee24bd6efae2ec1
Lyndon White <oxinabox@ucc.asn.au>
2020-12-17 14:57:52 +0000

    Don't disable doctests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
 
 @testset "BlockDiagonals" begin
     # The doctests fail on x86, so only run them on 64-bit hardware
-    # Sys.WORD_SIZE == 64 && doctest(BlockDiagonals)
+    Sys.WORD_SIZE == 64 && doctest(BlockDiagonals)
     include("blockdiagonal.jl")
     include("base_maths.jl")
     include("linalg.jl")
