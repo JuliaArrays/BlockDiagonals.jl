a4d8a1c66f104601f39cf5e3cd365f58cc3cad3a
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-02-08 16:05:10 +0000

    modernise the tests

--- test/chainrules.jl
+++ test/chainrules.jl
@@ -1,15 +1,11 @@
 @testset "chainrules.jl" begin
     @testset "BlockDiagonal" begin
         x = [randn(1, 2), randn(2, 2)]
-        x̄ = [randn(1, 2), randn(2, 2)]
-        ȳ = Composite{typeof(BlockDiagonal(x))}(blocks=[randn(1, 2), randn(2, 2)])
-        rrule_test(BlockDiagonal, ȳ, (x, x̄))
+        test_rrule(BlockDiagonal, x)
     end
 
     @testset "Matrix" begin
         D = BlockDiagonal([randn(1, 2), randn(2, 2)])
-        D̄ = Composite{typeof(D)}((blocks=[randn(1, 2), randn(2, 2)]), )
-        Ȳ = randn(size(D))
-        rrule_test(Matrix, Ȳ, (D, D̄))
+        test_rrule(Matrix, D)
     end
 end
