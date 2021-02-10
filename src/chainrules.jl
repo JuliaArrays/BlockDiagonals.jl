aca77ef4ba4bdcd03ed07366ceb7ee0e902af3c2
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-02-10 10:26:07 +0000

    do not close over sizes

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -34,7 +34,7 @@ function ChainRulesCore.rrule(::typeof(*), bm::BlockDiagonal{T, V}, v::AbstractV
     low2s = [1, (1 .+ high2s)...]
 
     function bm_vector_mul_pullback(Δ)
-        blocks = [Δ[low1s[i]:high1s[i]] * v[low2s[i]:high2s[i]]' for i in 1:length(sizes)]
+        blocks = [Δ[low1s[i]:high1s[i]] * v[low2s[i]:high2s[i]]' for i in 1:length(bm.blocks)]
         return (
             NO_FIELDS,
             Composite{BlockDiagonal{T, V}}(;blocks),
