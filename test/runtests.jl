e660015e3b41abce90690d9ca43ad6db0cc26f08
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-02-16 09:15:34 +0000

    move to_vec to src

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,6 @@ using FiniteDifferences # For overloading to_vec
 using Test
 using LinearAlgebra
 
-function FiniteDifferences.to_vec(X::BlockDiagonal)
-    x, blocks_from_vec = to_vec(X.blocks)
-    BlockDiagonal_from_vec(x_vec) = BlockDiagonal(blocks_from_vec(x_vec))
-    return x, BlockDiagonal_from_vec
-end
-
 @testset "BlockDiagonals" begin
     # The doctests fail on x86, so only run them on 64-bit hardware
     Sys.WORD_SIZE == 64 && doctest(BlockDiagonals)
