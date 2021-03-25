f0f5678c5f090de2b6c424e4f1fa915c170fcc33
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-03-25 18:42:05 +0000

    Stop running docstests during test step (they are too fragile to the julia version)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,11 @@
 using BlockDiagonals
 using ChainRulesCore
 using ChainRulesTestUtils
-using Documenter
 using FiniteDifferences # For overloading to_vec
 using Test
 using LinearAlgebra
 
 @testset "BlockDiagonals" begin
-    # The doctests fail on x86, so only run them on 64-bit hardware
-    Sys.WORD_SIZE == 64 && doctest(BlockDiagonals)
     include("blockdiagonal.jl")
     include("base_maths.jl")
     include("chainrules.jl")
