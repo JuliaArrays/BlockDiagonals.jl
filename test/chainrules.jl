b17bf214cdf4f60f92636d4acd73f0560b94269f
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-08-03 17:27:10 +0200

    add tests

--- test/chainrules.jl
+++ test/chainrules.jl
@@ -15,4 +15,13 @@
         v = rand(6)
         test_rrule(*, D, v)
     end
+
+    @testset "ProjectTo" begin
+        bd = BlockDiagonal([ones(2, 2), ones(3, 3)])
+        project = ProjectTo(bd)
+        @test project(ones(5, 5)) == bd
+        @test project(reshape(ones(5, 5), 5, 5)) == bd
+        @test project(Diagonal(ones(5))) isa BlockDiagonal
+        @test project(Diagonal(ones(5))) == Diagonal(ones(5))
+    end
 end
