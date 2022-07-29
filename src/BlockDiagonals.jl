3de63b21d408cabf8120afcdb83a5613ffe1c294
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2022-07-29 16:15:12 +0200

    fix include order

--- src/BlockDiagonals.jl
+++ src/BlockDiagonals.jl
@@ -11,10 +11,10 @@ import ChainRulesCore.ProjectTo
 export BlockDiagonal, blocks
 export blocksize, blocksizes, nblocks
 
-include("deprecate.jl")
 include("blockdiagonal.jl")
 include("base_maths.jl")
 include("chainrules.jl")
 include("linalg.jl")
+include("deprecate.jl")
 
 end  # end module
