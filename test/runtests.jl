0a5ef2ec9f258e3ce988fa9a900668341a21d5cb
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2025-03-07 13:19:53 +0100

    Remove FiniteDifferences and make ChainRuelsCore an extension

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,20 @@
 using BlockDiagonals
-using ChainRulesCore
-using ChainRulesTestUtils
+
 using Documenter
-using FiniteDifferences # For overloading to_vec
 using PDMats
 using Test
 using LinearAlgebra
+using ChainRulesCore
+using ChainRulesTestUtils
 
 push!(ChainRulesTestUtils.TRANSFORMS_TO_ALT_TANGENTS, x -> @thunk(x))
 
 @testset "BlockDiagonals" begin
-    # The doctests fail version other than 64bit julia 1.6.x, due to printing differences
-    Sys.WORD_SIZE == 64 && v"1.6" <= VERSION < v"1.7" && doctest(BlockDiagonals)
     include("blockdiagonal.jl")
     include("base_maths.jl")
-    include("chainrules.jl")
     include("linalg.jl")
+    include("chainrules.jl")
 end  # tests
+
+#
+#tests
